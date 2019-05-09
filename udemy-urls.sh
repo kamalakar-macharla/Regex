4a488e18cff1f53eec85c7982a2bdd7995174a60
kamalakar-macharla <kamalakar-macharla@users.noreply.github.com>
2019-05-09 04:03:47 -0700

    Create udemy-urls.sh

---
+++ udemy-urls.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+start chrome https://www.udemy.com/learn-devops-ci-cd-with-jenkins-using-pipelines-and-docker/learn/v4/t/lecture/7208638?start=0
+start firefox https://www.udemy.com/learn-devops-ci-cd-with-jenkins-using-pipelines-and-docker/learn/v4/t/lecture/7208638?start=0
