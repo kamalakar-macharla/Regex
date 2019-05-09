e6c89b5544d1a28688db95add1435d712f1c9021
kamalakar-macharla <kamalakar-macharla@users.noreply.github.com>
2019-05-09 04:14:42 -0700

    Update udemy-urls.sh

--- udemy-urls.sh
+++ udemy-urls.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-start chrome https://www.udemy.com/learn-devops-ci-cd-with-jenkins-using-pipelines-and-docker/learn/v4/t/lecture/7208638?start=0
-start firefox https://www.udemy.com/learn-devops-ci-cd-with-jenkins-using-pipelines-and-docker/learn/v4/t/lecture/7208638?start=0
+start chrome https://www.udemy.com/learn-linux-in-5-days/learn/v4/t/lecture/1417758?start=555
+
