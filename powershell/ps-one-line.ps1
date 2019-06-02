c5df8788499c0caefd7baafb816ca34efc4cfae8
kamal <kamalakar312@gmail.com>
2019-06-02 09:19:09 -0700

    xp

--- powershell/ps-one-line.ps1
+++ powershell/ps-one-line.ps1
@@ -2,30 +2,110 @@
 xxx-yyyyy | Get-Member
 xxx-yyyy  | select propertyname
 
+###########################################################################
+
+Get-ChildItem | Select-String -Pattern 'grep' | group path | select name | ForEach-Object { 
+   write-output $_.name
+   Get-Content $_.Name
+   sleep 5
+   #Get-Content -Path $_
+}
+
+
+############################################################################
+
 Get-ChildItem | Get-Member
 Get-ChildItem | select-string -Pattern "grep"
 
 
 Get-ChildItem | select-string -Pattern "grep" | Get-Member
-Get-ChildItem | select-string -Pattern "grep" | select filename
+cls
+Write-Output $(Get-ChildItem | select-string -Pattern "grep" | group path | select name)
+
+$myvar=$(Get-ChildItem | select-string -Pattern "grep" | group filename | select name)
+cls
+echo $myvar | Get-Content {$_ name}
+
+Foreach ($i in $myvar){
+
+ Get-Content .\$i
+
+}
+pwd
+cd ..
+
+cd .\test
+
+Get-ChildItem | Get-Content
+Get-ChildItem | select-string -Pattern "grep" | Where-Object filename | Get-Content
+
+Get-ChildItem | select-string -Pattern "grep" | Where-Object filename | Get-Content
+
+Get-ChildItem | select-string -Pattern "grep" | group filename | Where name -Match '*.txt' | Get-Content
+
+Get-ChildItem | select-string -Pattern "grep" | group filename | where name -Match testingfile.txt
+
+Get-ChildItem | select-string -Pattern "grep" | Get-Content
+Get-ChildItem | select-string -Pattern "grep" | select filename | Get-Member
 Get-ChildItem | select-string -Pattern "grep" | group filename
 
 Get-ChildItem | select-string -Pattern "grep" | group filename | Get-Member
 Get-ChildItem | select-string -Pattern "grep" | group filename | select name
 grep -l 'grep' *.*
 
-######################################################################################
-ll | grep '.txt'
-Get-ChildItem | Where-Object name -Like *.txt
 ####################################################
+ll | grep '.txt'
+Get-ChildItem | Where-Object name -Like *.txt | select name
+
+####################################################
+GREP-ish
+cat DATA.TXT | where { $_ -match "Mary"}
+SED-ish
+cat DATA.TXT | % { $_ -replace "Mary","Susan" }
+Get-ChildItem .\test | Select-String -Pattern 'jiomee' | Get-Content | % { $_ -replace "jiomee","XXYYZZ" } | Set-Content .\xyz2.txt
+####################################################
+Get-Process | select processname                   # this return all objects
+Get-Process | where processname -Match 'wmi*'      # this return match objects
+##############################################
+
 
 pwd
-cd E:\GitHub-Repos\mastermind
+cd E:\GitHub-Repos\mastermind\powershell
 
 
+cls
+Get-ChildItem | Select-String -Pattern 'white' | Get-Member
+Get-ChildItem | Select-String -Pattern 'white' | Select-Object filename 
+Get-ChildItem | Select-String -Pattern 'white' | Select-Object filename | Get-Member
+
+Get-ChildItem | Select-String -Pattern 'white' | Select-Object filename | Get-Content
+
+Get-ChildItem | Select-String -Pattern 'white' | Select-Object filename | Get-Content  | % { $_ -replace "white","BLACK" }
+
+Get-ChildItem | Select-String -Pattern 'white' | % { $_ -replace "white","BLACK" }
+
+Get-Help Select-String -Online
+
+
+Get-ChildItem | Select-String -Pattern 'white' | Select-Object -Last 1 | Get-Content
+
+Get-ChildItem | Select-String -Pattern 'white' | Select-Object | % { $_ -replace "white","BLACK" }
+
+Get-ChildItem | Select-String -Pattern 'white' | % { $_ -replace "white","BLACK" }
+
+Get-ChildItem .\test | Select-String -Pattern 'jiomee' | Get-Content | % { $_ -replace "jiomee","XXYYZZ" } | Set-Content .\xyz2.txt
+
+pwd
+Get-ChildItem | 
+
+cat .\textfile.txt | Where -Match
+cat .\textfile.txt | where { $_ -match 'little' }
+
+cat .\textfile.txt | %{ $_ -replace "little","litt-one"}
 
 Get-Command -verb enable
 Get-Command | select -First 4 | select name
+
 Get-Command | select source
 
 Get-Command | Where-Object Name -Like *remoting
