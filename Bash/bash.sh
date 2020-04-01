68e86476bedf46157f310517bf15604feee923fe
Kamalakar Macharla <Kamalakar_Macharla@manulife.ca>
2020-04-01 17:29:17 +0530

    update

--- Bash/bash.sh
+++ Bash/bash.sh
@@ -800,3 +800,19 @@ grep -A 5 '\- name\: build-DR-test' pipeline.yml  # this prints the next 5 line 
 The non zero output indicate that file is empty.
 [ -s /tmp/f2 ]
 echo $?
+
+------------------ Handling file input arguments or parameters
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+fi
+The $# variable will tell you the number of input arguments the script was passed.
+
+Or you can check if an argument is an empty string or not like:
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+fi
+The -z switch will test if the expansion of "$1" is a null string or not. If it is a null string then the body is executed.
+-------------------
